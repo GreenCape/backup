fb7d5cf5bcaf495f6f1ed9b4625c8a1d10bc4ad9
Niels Braczek <nbraczek@bsds.de>
2013-09-24 02:24:06 +0200

    Added dump directory

--- source/backup-packages.sh
+++ source/backup-packages.sh
@@ -9,15 +9,55 @@ then
     exit 1;
 fi
 
+# Get option arguments
+has_error="no"
+directory="backup"
+
+while getopts ":d" opt
+do
+    case $opt in
+        d)  # directory
+            directory=$OPTARG
+            ;;
+        \?) # unknown
+            has_error="yes"
+            ;;
+    esac
+done
+
+# Error check
+if [ "$has_error" == "yes" ]
+then
+    echo ""
+    echo "Usage: "$(basename $0)" [-d directory]"
+    echo ""
+    echo "  -d directory   The target directory for the package dump."
+    echo "                 If ommitted, 'backup' is used."
+    echo "                 If the path is relative, i.e. not starting with a slash '/',"
+    echo "                 it is relative to the current directory."
+    echo ""
+    exit 1
+fi
+
+# Ensure presence
+if [ "${directory:0:1}" != "/" ]
+then
+    directory=$(pwd)"/"$directory
+fi
+if ! [[ -e $directory ]]
+then
+    mkdir $directory
+fi
+
 # Create a package list for restore
-dpkg --get-selections | awk '!/deinstall|purge|hold/ {print $1}' > packages.list
+dpkg --get-selections | awk '!/deinstall|purge|hold/ {print $1}' > $directory"/"packages.list
 
 # Get package states
-apt-mark showauto > package-states-auto
-apt-mark showmanual > package-states-manual
+apt-mark showauto > $directory"/"package-states-auto
+apt-mark showmanual > $directory"/"package-states-manual
 
 # Save package sources
-find /etc/apt/sources.list* -type f -name '*.list' -exec bash -c 'echo -e "\n## $1 ";grep "^[[:space:]]*[^#[:space:]]" ${1}' _ {} \; > sources.list
+find /etc/apt/sources.list* -type f -name '*.list' -exec bash -c 'echo -e "\n## $1 ";grep "^[[:space:]]*[^#[:space:]]" ${1}' _ {} \; > $directory"/"sources.list
 
 # Get the trusted keys
-cp /etc/apt/trusted.gpg trusted-keys.gpg
+cp /etc/apt/trusted.gpg $directory"/"trusted-keys.gpg
