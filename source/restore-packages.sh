dacf1a731fc0065729ed5a0cec39624ec0826782
Niels Braczek <nbraczek@bsds.de>
2013-09-24 02:30:28 +0200

    Added dump directory

--- source/restore-packages.sh
+++ source/restore-packages.sh
@@ -9,16 +9,57 @@ then
     exit 1;
 fi
 
+# Get option arguments
+has_error="no"
+directory="backup"
+
+while getopts ":d" opt
+do
+    case $opt in
+        d)  # directory
+            directory=$OPTARG
+            ;;
+        \?) # unknown
+            has_error="yes"
+            ;;
+    esac
+done
+
+# Error check
+if [ "$has_error" == "yes" ]
+then
+    echo ""
+    echo "Usage: "$(basename $0)" [-d directory]"
+    echo ""
+    echo "  -d directory   The target directory with the package dump."
+    echo "                 If ommitted, 'backup' is used."
+    echo "                 If the path is relative, i.e. not starting with a slash '/',"
+    echo "                 it is relative to the current directory."
+    echo ""
+    exit 1
+fi
+
+# Ensure presence
+if [ "${directory:0:1}" != "/" ]
+then
+    directory=$(pwd)"/"$directory
+fi
+if ! [[ -e $directory ]]
+then
+    echo "Unable to locate package dump. Directory '"$directory"' not found." >&2
+    exit 1;
+fi
+
 # Sync the source list
-cp sources.list /etc/apt/sources.list.d/restore.list
+cp $directory"/"sources.list /etc/apt/sources.list.d/restore.list
 
 # Import the trusted keys
-apt-key add trusted-keys.gpg
+apt-key add $directory"/"trusted-keys.gpg
 apt-get update
 
 # Install packages
-xargs -a "packages.list" apt-get install
+xargs -a $directory"/packages.list" apt-get install
 
 # Restore package states
-xargs -a "package-states-auto" apt-mark auto
-xargs -a "package-states-manual" apt-mark manual
+xargs -a $directory"/package-states-auto" apt-mark auto
+xargs -a $directory"/package-states-manual" apt-mark manual
